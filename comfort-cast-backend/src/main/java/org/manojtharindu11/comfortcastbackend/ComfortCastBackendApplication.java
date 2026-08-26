9a472dd540e948328a1154ccfd11b4bf19c91c75
manojtharindu11 <manojtharindu11@gmail.com>
2026-08-26 19:45:15 +0530

    chore: initialize the backend

---
+++ comfort-cast-backend/src/main/java/org/manojtharindu11/comfortcastbackend/ComfortCastBackendApplication.java
@@ -0,0 +1,13 @@
+package org.manojtharindu11.comfortcastbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfortCastBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComfortCastBackendApplication.class, args);
+    }
+
+}
---
+++ comfort-cast-backend/src/test/java/org/manojtharindu11/comfortcastbackend/ComfortCastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.manojtharindu11.comfortcastbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfortCastBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
