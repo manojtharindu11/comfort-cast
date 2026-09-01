90339a6599684d50f2087901b824718731355654
manojtharindu11 <manojtharindu11@gmail.com>
2026-09-01 15:11:37 +0530

    feat: update security configuration to permit access to /api/cities

--- comfort-cast-backend/src/main/java/org/manojtharindu11/comfortcastbackend/config/SecurityConfig.java
+++ comfort-cast-backend/src/main/java/org/manojtharindu11/comfortcastbackend/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                         )
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/cities").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 )
